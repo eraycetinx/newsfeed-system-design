557bbbc07b6c56e8c25705f4707f6efbcb7bcd91
ErayCetinX <cetin.eray@hotmail.com>
2026-06-22 22:45:53 +0300

    mock data schema modified

--- types/MockData.ts
+++ types/MockData.ts
@@ -1,6 +1,42 @@
-export interface MockData {
+export interface User {
   uuid: string;
+  createdAt: Date;
+}
+
+export interface Post {
+  uuid: string;
+  score: number;
+  userUuid: string;
+  commentCount: number;
+  likeCount: number;
+  createdAt: Date;
+}
+
+export interface Comment {
+  uuid: string;
+  postUuid: string;
   userUuid: string;
   createdAt: Date;
-  content: string;
+}
+
+export interface Like {
+  uuid: string;
+  postUuid: string;
+  userUuid: string;
+  createdAt: Date;
+}
+
+export interface Follow {
+  uuid: string;
+  followerUserUuid: string;
+  followedUserUuid: string;
+  createdAt: Date;
+}
+
+export interface DataType {
+  userData: User[];
+  postData: Post[];
+  commentData: Comment[];
+  likeData: Like[];
+  followData: Follow[];
 }
